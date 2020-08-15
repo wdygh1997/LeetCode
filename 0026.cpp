c65fc2cdb3e6901b713ddaa39aa33e37d3b14e11
萬艼芋 <1214833800@qq.com>
2020-08-15 15:30:55 +0800

    Create
    2020/8/15

---
+++ 0026.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        int i = 0, j = 1;
+        for(; i < nums.size(); i++) {
+            for(; j < nums.size(); j++) {
+                if (nums[j] != nums[i]){
+                    break;
+                }
+            }
+            if(j == nums.size())
+                return i+1;
+            nums[i+1] = nums[j];
+        }
+        return i+1;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        int ret = Solution().removeDuplicates(nums);
+
+        string out = integerVectorToString(nums, ret);
+        cout << out << endl;
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # LeetCode
  
+## 这是什么？
+
+LeetCode个人题解（持续更新中。。。），使用C++编写，cpp文件名对应LeetCode题号。
+
+## 如何运行？
+
+在本地：直接编译运行即可
+
+在LeetCode：仅将Solution类复制后提交即可
