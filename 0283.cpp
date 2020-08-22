c07878dfe84af8f1774ca5e6ba295f376a1eb5d7
萬艼芋 <1214833800@qq.com>
2020-08-22 11:06:55 +0800

    Create
    2020/8/22

---
+++ 0283.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) {
+                cnt++;
+                continue;
+            } else {
+                nums[i-cnt] = nums[i];
+            }
+        }
+        for(int i = nums.size()-cnt; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        Solution().moveZeroes(nums);
+
+        string out = integerVectorToString(nums);
+        cout << out << endl;
+    }
+    return 0;
+}
