025c91fb51aec0bb5e037707562c125aa84c8524
萬艼芋 <1214833800@qq.com>
2020-09-02 19:37:20 +0800

    Create
    2020/9/2

---
+++ 0021.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL && l2 == NULL)
+            return NULL;
+        if(l1 != NULL && l2 == NULL)
+            return l1;
+        if(l1 == NULL && l2 != NULL)
+            return l2;
+        ListNode *head, *tail;
+        if(l1->val <= l2->val) {
+            head = l1;
+            tail = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            tail = l2;
+            l2 = l2->next;
+        }
+        while(l1 != NULL && l2 != NULL) {
+            if(l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+                tail = tail->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+                tail = tail->next;
+            }
+        }
+        if(l1 != NULL) {
+            tail->next = l1;
+        } else {
+            tail->next = l2;
+        }
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* l1 = stringToListNode(line);
+        getline(cin, line);
+        ListNode* l2 = stringToListNode(line);
+
+        ListNode* ret = Solution().mergeTwoLists(l1, l2);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
