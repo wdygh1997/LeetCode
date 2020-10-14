854930de68343b307f6a308efb6038f0fbf6c610
萬艼芋 <1214833800@qq.com>
2020-10-14 12:28:30 +0800

    Create
    2020/10/14

---
+++ 0328.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr)
+            return nullptr;
+        ListNode *p = head, *h2 = nullptr, *t2;
+        int flag = 1;
+        while(p->next != nullptr) {
+            if(flag == 0)
+                p = p->next;
+            else {
+                if(h2 == nullptr) {
+                    h2 = p->next;
+                    t2 = h2;
+                } else {
+                    t2->next = p->next;
+                    t2 = t2->next;
+                }
+                p->next = p->next->next;
+                t2->next = nullptr;
+            }
+            flag = 1-flag;
+        }
+        p->next = h2;
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+
+        ListNode* ret = Solution().oddEvenList(head);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
